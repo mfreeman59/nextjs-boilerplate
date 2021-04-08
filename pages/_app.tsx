0f8542a8af05e00c9c455afbab88b08d8d307d4f
Kengo,MATSUDA <k.matsuda622@gmail.com>
2021-04-08 17:44:48 +0900

    ベースのCSS設定追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
+import { CssBaseline, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
 import React from 'react';
 import '../styles/globals.css';
 
+const theme = createMuiTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: '#1A9270',
+    },
+    secondary: {
+      main: '#1A9270',
+    },
+  },
+});
+
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import Document, {
   Head, Html, Main, NextScript,
 } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
@@ -23,5 +19,25 @@ class MyDocument extends Document {
     );
   }
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () => originalRenderPage({
+    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+  });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
 
 export default MyDocument;
