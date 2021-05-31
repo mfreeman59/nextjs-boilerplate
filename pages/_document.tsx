f019ddd6d14a2b56be40dcff81d3b38e15465d05
Kengo,MATSUDA <k.matsuda622@gmail.com>
2021-05-31 16:49:10 +0900

    fix ServerStyleSheets bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Document, {
   Head, Html, Main, NextScript,
 } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 class MyDocument extends Document {
   render() {
