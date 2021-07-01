18a7f62b622d9497cc05fec6df3de2fbce4eb0d6
Kengo,MATSUDA <k.matsuda622@gmail.com>
2021-07-01 09:24:33 +0900

    add favicon info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,17 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+          {/* favicon */}
+          {/* https://realfavicongenerator.net/ にて作成 */}
+          {/* /public/favicon/ に入れる */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#332188" />
+          <meta name="msapplication-TileColor" content="#332188" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
